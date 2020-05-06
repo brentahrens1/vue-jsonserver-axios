cffdca105b8675a2f8fb8c5257437c813daf9e42
Brent Ahrens <cidida@gmail.com>
2020-05-06 12:17:24 -0700

    vue with json-server and axios

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,44 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Todos</h1>
+    <input type="text" v-model="todosName" @keyup.enter="addToDo">
+    <ul>
+      <li v-for="todo of todos" :key="todo.id">
+        {{ todo.name }}
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from 'axios'
 
+const baseURL = 'http://localhost:3000/todos'
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      todos: [],
+      todosName: ''
+    }
+  },
+  async created() {
+    try {
+      const res = await axios.get(baseURL)
+
+      this.todos = res.data
+    } catch(e) {
+      console.log(e)
+    }
+  },
+  methods: {
+    async addToDo() {
+      const res = await axios.post(baseURL, { name: this.todosName })
+
+      this.todos = [...this.todos, res.data]
+
+      this.todosName = ''
+    }
   }
 }
 </script>
@@ -19,10 +46,27 @@ export default {
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  font-size: 1.5rem;
+}
+li {
+  list-style: none;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+}
+input {
+  padding: .5rem;
+  width: 12rem;
+  border-radius: 5px;
+  border: 1px solid #000;
+  outline: none;
+  font-size: 1.5rem;
+}
+ul {
+  margin: 0;
+  margin-top: 1rem;
+  padding: 0;
 }
 </style>
